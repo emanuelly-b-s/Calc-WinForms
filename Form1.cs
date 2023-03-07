c7c0f7325875ed86c656eef4c1288948ebe1d1b0
Emanuelly B. Silva <emanuellybs14@gmail.com>
2023-03-07 20:56:11 -0300

    tentando aprender windowns forms

---
+++ Form1.cs
@@ -0,0 +1,25 @@
+namespace WinFormsApp1Calc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
